0d616bbe12ccf56a0386140cc4a39d13a278a982
EightyFor <j13811593@163.com>
2026-07-02 20:45:54 +0800

    rewrite-phase5.1

--- packages/manager/src/components/editor/ISlidesBody.ts
+++
@@ -1,8 +0,0 @@
-import type { Ref } from 'vue'
-import type { IEditorBody } from './IEditorBody'
-
-export interface ISlidesBody extends IEditorBody {
-  readonly previewMode: Ref<'single' | 'all'>
-  readonly showThumbnailsLocal: Ref<boolean>
-  handleToolAction: (action: string) => void
-}
--- packages/manager/src/vite-env.d.ts
+++
@@ -1,7 +0,0 @@
-/// <reference types="vite/client" />
-
-declare module 'markdown-it-footnote' {
-  import type { PluginSimple } from 'markdown-it'
-  const fn: PluginSimple
-  export default fn
-}
